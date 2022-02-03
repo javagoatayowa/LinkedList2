471750dc651204630aefe7f1d56bdb9eb38f8380
Aneeq <412710@bsd48.org>
2022-02-02 22:46:42 -0800

    Second push

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 void add(Student* s, Node* & head, Node* previous, Node* current, int idtres);
 void printList(Node* firstNode);
+void remove(Node* &firstNode, Node* p, Node* c, int theID);//methods
 int main() {//main
   Node* head = NULL;//original head
   cout<<"hey, welcome to student list. to add a student, type ADD. to print, type PRINT, average to get an average of the gpas, and to delete, type DELETE!";
@@ -39,6 +40,13 @@ int main() {//main
       if(strcmp(input,"PRINT") == 0) {
 	printList(head);
       }
+       if(strcmp(input,"DELETE") == 0) {//delete
+        cout<<"What is id of the student you want to delete?";
+        cout<<endl;
+        int theIDID;
+        cin>>theIDID;
+        remove(head,head,head,theIDID);
+      }
     }
   }
   return 0;
@@ -69,5 +77,27 @@ void printList(Node* firstNode) {
   firstNode->getStudent()->printStudent();
   if(firstNode->getNext() != NULL) {
     printList(firstNode->getNext());//using recursion to go through and print.
+  }
+}
+void remove(Node* &firstNode, Node *p, Node* c, int theID) {
+  if(firstNode == NULL) {
+    cout<<"Empty LinkedList.";//check if empty
+  }
+  else if(theID == c->getStudent()->id) {//check if the id is found
+    if (c == firstNode) {
+      firstNode = firstNode->getNext();
+      c->setNext(NULL);
+      c->~Node();
+      return;
+    }
+    else {
+      p->setNext(c->getNext());
+      c->setNext(NULL);
+      c->~Node();
+      return;
+    }
+  }
+  else {//continuing with recursion
+    remove(firstNode, c, c->getNext(),theID);
   }
 }
